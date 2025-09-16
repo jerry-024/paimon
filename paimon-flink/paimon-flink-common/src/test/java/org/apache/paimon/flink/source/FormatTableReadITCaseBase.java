e459aa386e215f68898a27167bea096ee54157de
yantian <lining.jln@alibaba-inc.com>
2025-09-16 14:22:46 +0800

    update test

--- paimon-flink/paimon-flink-common/src/test/java/org/apache/paimon/flink/source/FormatTableReadITCaseBase.java
+++ paimon-flink/paimon-flink-common/src/test/java/org/apache/paimon/flink/source/FormatTableReadITCaseBase.java
@@ -19,16 +19,9 @@
 package org.apache.paimon.flink.source;
 
 import org.apache.paimon.catalog.Identifier;
-import org.apache.paimon.data.GenericRow;
-import org.apache.paimon.data.InternalRow;
+import org.apache.paimon.data.Decimal;
 import org.apache.paimon.flink.RESTCatalogITCaseBase;
-import org.apache.paimon.format.FileFormatFactory;
-import org.apache.paimon.format.FormatWriterFactory;
-import org.apache.paimon.format.parquet.ParquetFileFormatFactory;
-import org.apache.paimon.options.Options;
 import org.apache.paimon.rest.RESTToken;
-import org.apache.paimon.types.DataTypes;
-import org.apache.paimon.types.RowType;
 
 import org.apache.paimon.shade.guava30.com.google.common.collect.ImmutableMap;
 
@@ -36,6 +29,7 @@ import org.apache.flink.types.Row;
 import org.junit.jupiter.api.Test;
 
 import java.io.IOException;
+import java.math.BigDecimal;
 import java.util.UUID;
 
 import static org.assertj.core.api.Assertions.assertThat;
@@ -45,24 +39,12 @@ public class FormatTableReadITCaseBase extends RESTCatalogITCaseBase {
 
     @Test
     public void testParquetFileFormat() throws IOException {
-        FileFormatFactory formatFactory = new ParquetFileFormatFactory();
-        RowType rowType =
-                RowType.builder()
-                        .field("a", DataTypes.INT())
-                        .field("b", DataTypes.INT())
-                        .field("c", DataTypes.INT())
-                        .build();
-        FormatWriterFactory factory =
-                (formatFactory.create(
-                                new FileFormatFactory.FormatContext(new Options(), 1024, 1024)))
-                        .createWriterFactory(rowType);
-        InternalRow[] datas = new InternalRow[2];
-        datas[0] = GenericRow.of(1, 1, 1);
-        datas[1] = GenericRow.of(2, 2, 2);
+        String bigDecimalStr = "10.001";
+        Decimal decimal = Decimal.fromBigDecimal(new BigDecimal(bigDecimalStr), 8, 3);
         String tableName = "format_table_test";
         Identifier identifier = Identifier.create("default", tableName);
         sql(
-                "CREATE TABLE %s (a INT, b INT, c INT) WITH ('file.format'='parquet', 'type'='format-table')",
+                "CREATE TABLE %s (a DECIMAL(8, 3), b INT, c INT) WITH ('file.format'='parquet', 'type'='format-table')",
                 tableName);
         RESTToken expiredDataToken =
                 new RESTToken(
@@ -70,9 +52,11 @@ public class FormatTableReadITCaseBase extends RESTCatalogITCaseBase {
                                 "akId", "akId-expire", "akSecret", UUID.randomUUID().toString()),
                         System.currentTimeMillis() + 1000_000);
         restCatalogServer.setDataToken(identifier, expiredDataToken);
-        sql("INSERT INTO %s VALUES (1, 1, 1), (2, 2, 2)", tableName);
-        assertThat(sql("SELECT a FROM %s", tableName))
-                .containsExactlyInAnyOrder(Row.of(1), Row.of(2));
+        sql("INSERT INTO %s VALUES (%s, 1, 1), (%s, 2, 2)", tableName, decimal, decimal);
+        assertThat(sql("SELECT a, b FROM %s", tableName))
+                .containsExactlyInAnyOrder(
+                        Row.of(new BigDecimal(bigDecimalStr), 1),
+                        Row.of(new BigDecimal(bigDecimalStr), 2));
         sql("Drop TABLE %s", tableName);
     }
 }
